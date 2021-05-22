2331e4bfee8ec0e7ab438d4727c368de4937d47f
AmesisProject <57772906+AmesisProject@users.noreply.github.com>
2021-05-22 10:47:05 +0200

    correction
    correction des commentaire et du pin d'entré pour accueuillir la pédale et le tps

--- Throttle pedale Arduino/Amesis_throttleBady/Amesis_throttleBody/Amesis_throttleBody.ino
+++ Throttle pedale Arduino/Amesis_throttleBady/Amesis_throttleBody/Amesis_throttleBody.ino
@@ -8,34 +8,36 @@ Discord   : https://discord.gg/vRAybmd3hk
 FaceBook  : https://www.facebook.com/amesis.pro.5
 Instagram : https://www.instagram.com/?hl=fr
   
-  licence OpenSource non comercialisable, modifiable en citant le propriétaire AmesisProject avec l'un des liens ci dessus
+  licence OpenSource non comercialisable, modifiable en citant 
+  le propriétaire AmesisProject avec l'un des liens ci dessus
   Sur base de l'exmple arduino AnalogInOutSerial
-  
-NomDuProjet : Amesis_throttle 
-Version     : v1.0 18/05/2021
-ButDuProjet : Renvoyer la valeur de la pédale (du groupe VAG "VR5 AQN" de 0 à 100 % sur le port serie.
+
+002-Amesis-Throttle_To_ThrottleBody_Arduino
+NomDuProjet : Amesis_throttleBody 
+Version     : v1.0 20/05/2021
+ButDuProjet : Renvoyer la valeur du potentiometre (TPS) du papillon motorisé  (throttle body)du groupe VAG "VR5 AQN" de 0 à 100 % sur le port serie.
 ProjetDisponibleIciGitHub : https://github.com/AmesisProject/002-Amesis-Throttle_To_ThrottleBody_Arduino/tree/main/Throttle%20pedale%20Arduino
 
-Merci de faire suivre sur github toute modification au amélioration du code.
+Merci de faire suivre sur github toute modification ou amélioration du code.
 
 Branchement :
 
- Pedale   /  Calculateur   /   ArduinoMega    
- 1            72               5v
- 2            -                -               
- 3            -                -              
- 4            -                -               
- 5            33               Gnd
- 6            34               A0
+ ThrottleBody   /  Calculateur   /   ArduinoMega    
+ 1                  -                 -
+ 2                  83                Gnd               
+ 3                  -                 -              
+ 4                  84                A1               
+ 5                  -                 -
+ 6                  91                +5V
 
 */
 //////////////////////////////
 // Déclaration des variable //
 //////////////////////////////
 
-const int PedalePin = A0;             // Pin analogique d'enté (input) du potentiometre
-      int Pedale = 0;                 // lire la vleur du potentiometre
-      int PedaleValeur = 0;           // valeur entre 0 et 100%
+const int TpsPin = A1;                // Pin analogique d'enté (input) du potentiometre tps
+      int TpsNumerique = 0;                 // lire la vleur du potentiometre en numerique de 0 à 1023
+      int TpsValeur = 0;           // valeur entre 0 et 100% convertie depuis la valeur 0 à 1023
 
       
 ///////////////////////////////
@@ -53,14 +55,14 @@ void setup() {
 
 void loop() {
   // Lectue de la valeur annalogique:
-  Pedale = analogRead(PedalePin);
+  TpsNumerique = analogRead(TpsPin);
   
   // Fonction "map" de mise à l'echelle : le 70 et 474 sont les valeur de ma pédale, il se peut que d'autre pédale et des valeur differente, pour les trouver remplacer les valeur "PedaleValeur = map(Pedale, X, Y, 0, 100);" X et Y par X = 0 et Y = 1023 et controlez votre debut/fin de course de votre pédale, vous trouvereai votre propre X et Y 
-  PedaleValeur = map(Pedale, 82, 919, 0, 100);
+  TpsValeur = map(TpsNumerique, 82, 919, 0, 100);
   
   // On renvoi les info sur le port serie pour la consol arduino
-  Serial.print("Position de la pedale est à = ");
-  Serial.print(PedaleValeur);
+  Serial.print("Position du boitier papillon (TPS) est à = ");
+  Serial.print(TpsValeur);
   Serial.println("%");
 
   // On attend 2 miliseconde pour que notre montage soit stable 
